249edc05ddd94ff76273e60bf4348bbbcb0c4c24
adcouch <60738463+adcouch@users.noreply.github.com>
2022-07-19 13:50:49 +0100

    Update custom_1.sh

--- setup/custom_1.sh
+++ setup/custom_1.sh
@@ -17,23 +17,20 @@ echo "deb [arch=amd64] https://packages.microsoft.com/repos/microsoft-debian-str
 for i in sipcalc veil rstat-client cifs-utils oscanner rusers filezilla ipmitool freeipmi htop iftop wondershaper libssl-dev libffi-dev python-dev build-essential nfs-common veil rsh-client python3-pip python-pip; do
   apt install $i -y
 done
-"Post_Deployment_Script: additional packages added"
+logger "Post_Deployment_Script: additional packages added"
 
-git clone https://github.com/byt3bl33d3r/CrackMapExec
-cd CrackMapExec && git submodule init && git submodule update --recursive
-python setup.py install
-"Post_Deployment_Script: cme added from repo"
+cd /opt
 
 git clone https://github.com/byt3bl33d3r/SprayingToolkit.git
-cd SprayingToolkit $$ pip3 install -r requirements.txt
-"Post_Deployment_Script: SprayingToolkit added"
+cd SprayingToolkit && pip3 install -r requirements.txt
+logger "Post_Deployment_Script: SprayingToolkit added"
 
 git clone https://github.com/m8r0wn/nullinux
 cd nullinux/
 bash setup.sh
-"Post_Deployment_Script: nullinux added"
+logger "Post_Deployment_Script: nullinux added"
 
-cd /home/kali
+cd /opt
 
 git clone https://github.com/danielbohannon/Invoke-Obfuscation.git
 git clone https://github.com/leapsecurity/InSpy.git
@@ -42,24 +39,24 @@ git clone https://github.com/PowerShellMafia/PowerSploit.git
 
 
 pip install ldapdomaindump ldap3 dnspython
-"Post_Deployment_Script: additional python packages installed"
+logger "Post_Deployment_Script: additional python packages installed"
 
-cd /home/kali
+cd /opt
 git clone https://github.com/EmpireProject/Empire.git
 ./Empire/setup/install.sh
-cd /home/kali
-"Post_Deployment_Script: empire setup"
+cd /opt
+logger "Post_Deployment_Script: empire setup"
 
-https://github.com/Veil-Framework/Veil-Catapult.git
+git clone https://github.com/Veil-Framework/Veil-Catapult.git
 
-cd /home/kali
+cd /opt
 git clone https://github.com/carlospolop/privilege-escalation-awesome-scripts-suite.git
-"Post_Deployment_Script: win and lin peas added"
+logger "Post_Deployment_Script: win and lin peas added"
 
 git clone https://github.com/rebootuser/LinEnum.git
-"Post_Deployment_Script: linenum added"
+logger "Post_Deployment_Script: linenum added"
 
-cd /home/kali
+cd /opt
 
 mkdir webshare
 cp LinEnum/LinEnum.sh webshare/
@@ -69,7 +66,7 @@ cp PowerSploit/Recon/PowerView.ps1 webshare/
 cp PowerSploit/Exfiltration/Invoke-Mimikatz.ps1 webshare/
 "Post_Deployment_Script: webshare created"
 
-cd /home/kali
+cd /opt
 
 wget https://labs.portcullis.co.uk/download/rdp-sec-check-0.9.tgz --no-check-certificate
 tar xvfz rdp-sec-check-0.9.tgz
@@ -86,12 +83,9 @@ make install
 
 cd /home/kali
 
-echo export HISTFILESIZE= >> .bashrc
-echo export HISTSIZE= >> .bashrc
-echo export HISTTIMEFORMAT=\"[%F %T]\" >> .bashrc
-echo export HISTFILE=\~/.bash_supersize_history >> .bashrc
-echo PROMPT_COMMAND=\"history -a; $PROMPT_COMMAND\" >> .bashrc
+echo export HISTFILESIZE= >> .zshrc
+echo export HISTSIZE= >> .zshrc
+echo export HISTTIMEFORMAT=\"[%F %T]\" >> .zshrc
+echo export HISTFILE=\~/.bash_supersize_history >> .zshrc
+echo PROMPT_COMMAND=\"history -a; $PROMPT_COMMAND\" >> .zshrc
 "Post_Deployment_Script: custom history added to rc file"
-
-echo PS1="\[\e]0;\u: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;34m\]\w\[\033[00m\]\$" >> .bashrc
-"Post_Deployment_Script: bash prompt changed"
